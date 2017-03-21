cee9be29b66a13b4a6f7ec6e88c81d29136682e8
zhaoxueping007 <zoeyzhao007@gmail.com>
2017-03-21 19:48:04 +0800

    show group's creator info

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! ,only: [:new, :create]
+  before_action :authenticate_user! , only: [:new, :create]
 
   def index
     @groups = Group.all
@@ -20,7 +20,7 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     @group.user = current_user
-    
+
     if @group.save
       redirect_to groups_path
     else
